cdec029c6f724cbf955377649b882dd3ae7a5f42
Ben Honeywill <bhnywl@gmail.com>
2020-01-15 19:47:57 +0000

    improved error state

--- components/error-message.js
+++ components/error-message.js
@@ -1,24 +1,13 @@
 import React from "react";
-import styled from "@emotion/styled";
 
-const Container = styled.blockquote`
-  max-width: 600px;
-  margin: 0;
-`;
-
-const Text = styled.p`
-  margin: 0;
-  font-size: 32px;
-  line-height: 1.3;
-`;
+import Blockquote from "./blockquote";
 
 const ErrorMessage = () => {
   return (
-    <Container>
-      <Text>
-        Sorry. We are having trouble getting any Stoic quotes right now, but do not worry, it is out of your control.
-      </Text>
-    </Container>
+    <Blockquote
+      text="Sorry. We are having trouble getting any Stoic quotes right now. However, do not worry. It is out of your control."
+      author="Ben Honeywill"
+    />
   );
 };
 
