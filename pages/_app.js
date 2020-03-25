b2528905ab30df0edad092102bc20eee26c2bdb0
Ben Honeywill <bhnywl@gmail.com>
2020-03-25 20:03:08 +0000

    meta tags for seo

--- pages/_app.js
+++ pages/_app.js
@@ -8,15 +8,54 @@ class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props;
 
+    const title = "Stoic Quotes | The best quotes from the great Roman Stoics";
+    const description =
+      "The very best Stoic quotes from the three great Roman Stoics: Marcus Aurelius, Seneca, and Epictetus. Presented in bitesize chunks, learn from their wisdom and bring Stoicism into your everyday life.";
+
     return (
       <>
         <Head>
-          <title>Stoic Quotes</title>
-          <meta name="theme-color" content="#0c0c0c" />
-          <meta
-            name="description"
-            content="Daily Stoic quotes from Marcus Aurelius, Seneca, and Epictetus; making it simple to bring Stoicism into your everyday life."
-          />
+          <title>{title}</title>
+          {[
+            {
+              name: "description",
+              content: description
+            },
+            {
+              property: "og:title",
+              content: title
+            },
+            {
+              property: "og:description",
+              content: description
+            },
+            {
+              property: "og:type",
+              content: "website"
+            },
+            {
+              name: "twitter:creator",
+              content: "benhoneywill"
+            },
+            {
+              name: "twitter:title",
+              content: title
+            },
+            {
+              name: "twitter:description",
+              content: description
+            },
+            {
+              name: "keywords",
+              content: "Stoic, Stoicism, Marcus, Aurelius, Seneca, Epictetus, Roman, Philosophy, Quotes, Inspirational"
+            },
+            {
+              name: "theme-color",
+              content: "#0c0c0c"
+            }
+          ].map(props => (
+            <meta {...props} />
+          ))}
         </Head>
 
         <GlobalStyle />
