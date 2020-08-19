b432bdb356a7453fad8801d8d3e6f17d03c26803
Ben Honeywill <bhnywl@gmail.com>
2020-08-19 20:24:03 +0100

    error handling for invalid num param

--- pages/api/quotes.js
+++ pages/api/quotes.js
@@ -2,7 +2,13 @@ import data from "../../data/quotes.json";
 import { shuffle } from "../../helpers/general";
 
 export default (req, res) => {
-  const num = req.query.num || 10; 
-  const quotes = shuffle(data.quotes).slice(0, Math.min(num, 100))
+  const { num = 10 } = req.query;
+  let count = parseInt(num, 10);
+
+  if (isNaN(count) || count <= 0 || count > 100) {
+    return res.status(422).json({ error: "`num` must be an integer from 1 to 100." })
+  }
+
+  const quotes = shuffle(data.quotes).slice(0, count)
   res.status(200).json(quotes);
 };
